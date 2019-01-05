3c5cd6b1d115cc0f4e941271cd2cd7d266fa3251
BanFcc <39665166+BanFcc@users.noreply.github.com>
2019-01-05 15:00:15 +0800

    Create head.cpp

---
+++ head.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define mp make_pair
+#define fi first
+#define se second
+#define debug(x) cerr<<#x<<" = "<<(x)<<endl
+#define nm(x) (#x)
+#define eps 1e-8
+#define pi acos(-1.0)
+using namespace std;
+void test(){cerr<<"\n";}
+template<typename T,typename... Args>void test(T x,Args... args){cerr<<x<<" ";test(args...);}
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+const int MAXN=(int)1e5+5;
+const int MOD=(int)1e9+7;
+int main()
+{
+    return 0;
+}
